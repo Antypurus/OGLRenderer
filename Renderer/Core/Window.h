daa6fd2e087e54646875873823b0f15c34902247
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-07-29 03:19:41 +0100

    get to work

--- Renderer/Core/Window.h
+++ Renderer/Core/Window.h
@@ -6,32 +6,27 @@
 #include <string>
 #include <memory>
 
-namespace VKR
+struct Window
 {
+    public:
+    std::string name = "";
+    uint32_t height;
+    uint32_t width;
+    GLFWwindow* window_handle = nullptr;
     
-    struct Window
-    {
-        public:
-        std::string name = "";
-        uint32_t height;
-        uint32_t width;
-        GLFWwindow* window_handle = nullptr;
-        
-        private:
-        bool is_open = true;
-        
-        public:
-        Window(uint32_t width, uint32_t height, const std::string& window_name);
-        Window(uint32_t width, uint32_t height, std::string&& window_name);
-        ~Window();
-        bool IsOpen();
-        
-        private:
-        /*Handles creating the GLFW window regardless of what constructor was used*/
-        void CreateWindow();
-        /*Polls Window Events*/
-        void PollEvents();
-        
-    };
+    private:
+    bool is_open = true;
     
-}
+    public:
+    Window(uint32_t width, uint32_t height, const std::string& window_name);
+    Window(uint32_t width, uint32_t height, std::string&& window_name);
+    ~Window();
+    bool IsOpen();
+    
+    private:
+    /*Handles creating the GLFW window regardless of what constructor was used*/
+    void CreateWindow();
+    /*Polls Window Events*/
+    void PollEvents();
+    
+};
--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -2,16 +2,14 @@
 
 #include "Core/Window.h"
 
-using namespace VKR;
-
 int main()
 {
     Window window = {800,600,"Window"};
-
+    
     while(window.IsOpen())
     {
-
+        
     }
-
+    
     return 0;
 }
