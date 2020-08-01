9090aaf64405bf79febdf6fe26775ec9f47082a6
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-01 20:39:39 +0100

    working on a vertex buffer abstraction

--- Renderer/Core/VertexBuffer.h
+++ Renderer/Core/VertexBuffer.h
@@ -1,10 +1,22 @@
 /* date = August 1st 2020 5:04 pm */
 #pragma once
 #include <cstdint>
+#include <vector>
 
+template<typename vertex_type>
 struct VertexBuffer
 {
-public:
+	public:
 	uint32_t vertex_buffer_object = 0;
+	std::vector<vertex_type> vertices;
+	
+	public:
+	VertexBuffer(const std::vector<vertex_type>& vertices)
+		:vertices(vertices)
+	{
+		glGenBuffers(1, &this->vertex_buffer_object);
+		glBindBuffer(GL_ARRAY_BUFFER, this->vertex_buffer_object);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertex_type), vertices.data(), GL_STATIC_DRAW);
+	}
 };
 
