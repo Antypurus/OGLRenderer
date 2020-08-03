a4f31e0dbf4b3bf78940d162107d1dfd5910d142
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-03 21:18:12 +0100

    working on transform class

--- Renderer/Transform.cpp
+++ Renderer/Transform.cpp
@@ -1,2 +1,29 @@
 #include "Transform.h"
+#include <glm/gtx/transform.hpp>
 
+#include "Core/Shader.h"
+
+using namespace glm;
+
+Transform::Transform(vec3 position, vec3 scale, vec3 rotation)
+:position(position), scale(scale), rotation(rotation)
+{
+}
+
+mat4 Transform::GenerateModelMatrix()
+{
+	mat4 results = mat4x4{};
+	results *= translate(this->position);
+	results *= glm::scale(this->scale);
+	//NOTE(Tiago): rotate in each of the axis
+	results *= rotate(this->rotation[0], vec3{1,0,0});
+	results *= rotate(this->rotation[1], vec3{0,1,0});
+	results *= rotate(this->rotation[2], vec3{0,0,1});
+	results *= translate(-this->position);
+	return results;
+}
+
+void Transform::Bind(const std::string& uniform_name, GPUProgram& shader)
+{
+	shader.SetMatrix4f(uniform_name, this->GenerateModelMatrix());
+}
--- Renderer/Transform.h
+++ Renderer/Transform.h
@@ -1,6 +1,9 @@
 /* date = August 3rd 2020 7:30 pm */
 #pragma once
+#include <string>
 #include <glm/glm.hpp>
+
+struct GPUProgram;
 
 struct Transform
 {
@@ -10,4 +13,5 @@ struct Transform
 	
 	Transform(glm::vec3 position, glm::vec3 scale, glm::vec3 rotation);
 	glm::mat4x4 GenerateModelMatrix();
+	void Bind(const std::string& uniform_name, GPUProgram& shader);
 };
