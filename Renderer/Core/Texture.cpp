7e7b2a7ef67f241cea2890f36d725260d3623d98
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-03 01:49:56 +0100

    fixed issues with stb image compilation

--- Renderer/Core/Texture.cpp
+++ Renderer/Core/Texture.cpp
@@ -1,2 +1,9 @@
 #include "Texture.h"
+#include <stb_image.h>
+
+Texture::Texture(const std::string& texture_path)
+{
+	unsigned char* texture_data = stbi_load(texture_path.c_str(), &this->width, &this->height, &this->channel_count, 0);
+	this->texture_data = std::unique_ptr<unsigned char>(texture_data);
+}
 
--- Renderer/Core/Texture.h
+++ Renderer/Core/Texture.h
@@ -1,9 +1,15 @@
 /* date = August 3rd 2020 1:13 am */
 #pragma once
-#include <stb_image.h>
+#include <memory>
+#include <string>
 
 struct Texture
 {
+	std::unique_ptr<unsigned char> texture_data = nullptr;
+	int width = 0;
+	int height = 0;
+	int channel_count = 0;
 	
+	Texture(const std::string& texture_path);
 };
 
