3fd49fa9816bee79e635c183b6a9de5714838645
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-04 19:35:09 +0100

    working on entity system

--- Renderer/Core/IndexBuffer.h
+++ Renderer/Core/IndexBuffer.h
@@ -7,8 +7,9 @@ struct IndexBuffer
 {
 	uint32_t index_buffer_object = 0;
 	const std::vector<uint32_t> indices;
-	
-	public:
+
+public:
+	IndexBuffer() = default;
 	IndexBuffer(const std::vector<uint32_t>& indices);
 	void Bind();
 	void Draw();
