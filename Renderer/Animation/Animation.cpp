15eafc909c5f2ca160a7e2b295fcff619e8c7ab5
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-04 01:27:05 +0100

    workin on animation system

--- Renderer/Animation/Animation.cpp
+++ Renderer/Animation/Animation.cpp
@@ -1,2 +1,33 @@
 #include "Animation.h"
 
+Keyframe::Keyframe(const uint32_t& delta_t, const Transform& transform_delta)
+:delta_t(delta_t),transform_delta(transform_delta)
+{
+}
+
+void Animation::AddKeyframe(const Keyframe& keyframe)
+{
+	this->keyframes.push_back(keyframe);
+	if(this->keyframes.size() == 1)
+	{
+		// NOTE(Tiago): fist keyframe added. Start time = 0
+		this->keyframes[0].start_time = 0;
+	}else
+	{
+		// NOTE(Tiago): not the first keyframe. Start time = Start Time of Previous Keyframe + previous keyframe duration
+		const uint32_t keyframe_index = this->keyframes.size() - 1;
+		const uint32_t start_time = this->keyframes[keyframe_index-1].start_time + this->keyframes[keyframe_index-1].delta_t; 
+		this->keyframes[keyframe_index].start_time = start_time;
+	}
+}
+
+void Animation::Play()
+{
+	this->playback_head = 0;
+	this->current_keyframe_index = 0;
+}
+
+Transform Animation::Interpolate(const Keyframe& start_keyframe, const Keyframe& end_keyframe, uint32_t playback_head)
+{
+	return {};
+}
--- Renderer/Animation/Animation.h
+++ Renderer/Animation/Animation.h
@@ -1,12 +1,30 @@
 /* date = August 4th 2020 0:38 am */
 #pragma once
+#include <cstdint>
+#include "../Transform.h"
+#include <vector>
 
 struct Keyframe
 {
+	uint32_t start_time = 0;
+	uint32_t delta_t = 0;
+	Transform transform_delta;
 	
+	Keyframe() = default;
+	Keyframe(const uint32_t& delta_t, const Transform& transform_delta);
 };
 
 struct Animation
 {
+	uint32_t playback_head = 0;
+	uint32_t current_keyframe_index =0;
+	std::vector<Keyframe> keyframes;
+	Transform current_transform;
 	
+	public:
+	Animation() = default;
+	void AddKeyframe(const Keyframe& keyframe);
+	void Play();
+	
+	static Transform Interpolate(const Keyframe& start_keyframe, const Keyframe& end_keyframe, uint32_t playback_head);
 };
