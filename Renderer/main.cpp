23ce237fd37965ea1ebd7d55c43b027c49af536b
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-04 20:15:45 +0100

    seems we have a perf issue to handle

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -16,6 +16,134 @@
 #include <atomic>
 #include "Entity.h"
 
+struct Game
+{
+	uint64_t credit_count = 0;
+	std::atomic_uint64_t play_count = 0;
+	uint64_t credits_extracted = 0;
+	std::vector<Entity> entities;
+	bool animating = false;
+
+	Game()
+	{
+		std::vector<Vertex> vertices = {
+			{glm::vec3{0.5f,  0.5f, 0.0f},  glm::vec2{1.0f, 1.0f}},  // top right
+			{glm::vec3{0.5f, -0.5f, 0.0f},  glm::vec2{1.0f, 0.0f}},  // bottom right
+			{glm::vec3{-0.5f, -0.5f, 0.0f}, glm::vec2{0.0f, 0.0f}},  // bottom left
+			{glm::vec3{-0.5f,  0.5f, 0.0f}, glm::vec2{0.0f, 1.0f}}   // top left 
+		};
+		std::vector<uint32_t> indices = {
+			0, 1, 3,   // first triangle
+			1, 2, 3    // second triangle
+		};
+		VertexBuffer<Vertex> vbo(vertices);
+		IndexBuffer ib(indices);
+		Texture texture = { "./Resources/Textures/test.jpg" };
+		Model model = {vbo, ib, texture};
+
+		Transform model_matrix = {
+			glm::vec3{0.0f,0.0f,0.0f},
+			glm::vec3{1.0f,1.0f,1.0f},
+			glm::vec3{0.0f,0.0f,0.0f}
+		};
+
+		constexpr uint64_t entity_count = 10;
+		for (uint64_t entity_index = 0; entity_index < entity_count; ++entity_index)
+		{
+			Animation animation;
+			animation.AddKeyframe(
+				{ 1000,
+				{
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,90.0f}
+				}
+				});
+			animation.AddKeyframe(
+				{ 1000,
+				{
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,-90.0f}
+				}
+				});
+			animation.AddKeyframe(
+				{ 1000,
+				{
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,90.0f}
+				}
+				});
+			animation.AddKeyframe(
+				{ 1000,
+				{
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,0.0f},
+					glm::vec3{0.0f,0.0f,0.0f}
+				}
+				});
+
+			Entity entity = { model_matrix, model, animation};
+
+			this->entities.push_back(entity);
+
+			//adds the callback only to the last entity
+			if (entity_index == entity_count - 1)
+			{
+				this->entities[this->entities.size() - 1].animation.AddAnimationEndCallback([this]()
+					{
+						this->animating = false;
+						this->play_count++;
+					});
+			}
+		}
+	}
+
+	void Start()
+	{
+		if (this->animating)
+		{
+			for(Entity& entity:entities)
+			{
+				entity.animation.PauseResumeToggle();
+			}
+		}
+		else
+		{
+			if (credit_count > 0)
+			{
+				this->animating = true;
+				credit_count--;
+				for (Entity& entity : entities)
+				{
+					entity.animation.Play();
+				}
+			}
+		}
+	}
+
+	void CreditsIn()
+	{
+		credit_count++;
+	}
+
+	void CreditsOut()
+	{
+		credits_extracted += credit_count;
+		credit_count = 0;
+	}
+
+	void Draw(GPUProgram& shader, const std::string& model_matrix_uniform_name)
+	{
+		for (Entity& entity : entities)
+		{
+			entity.Draw(shader, model_matrix_uniform_name);
+		}
+	}
+
+};
+
 void InitImGui(const Window& window)
 {
 	IMGUI_CHECKVERSION();
@@ -54,83 +182,11 @@ int main()
 	glewInit(); // Use GLEW to load modern OpenGL functions from the GPU driver, this must be done after creating the window.
 	InitImGui(window);
 
-	std::vector<Vertex> vertices = {
-		{glm::vec3{0.5f,  0.5f, 0.0f},  glm::vec2{1.0f, 1.0f}},  // top right
-		{glm::vec3{0.5f, -0.5f, 0.0f},  glm::vec2{1.0f, 0.0f}},  // bottom right
-		{glm::vec3{-0.5f, -0.5f, 0.0f}, glm::vec2{0.0f, 0.0f}},  // bottom left
-		{glm::vec3{-0.5f,  0.5f, 0.0f}, glm::vec2{0.0f, 1.0f}}   // top left 
-	};
-
-	std::vector<uint32_t> indices = {
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
-	};
-
-	VertexBuffer<Vertex> vbo(vertices);
-	IndexBuffer ib(indices);
-
 	Shader vs = { "./Resources/Shaders/base_vert.glsl", ShaderType::Vertex };
 	Shader fs = { "./Resources/Shaders/base_frag.glsl", ShaderType::Fragment };
 	GPUProgram shader = { vs, fs };
 
-	Texture texture = { "./Resources/Textures/test.jpg" };
-
-	Transform model_matrix = {
-		glm::vec3{0.0f,0.0f,0.0f},
-		glm::vec3{1.0f,1.0f,1.0f},
-		glm::vec3{0.0f,0.0f,0.0f}
-	};
-
-	Animation animation;
-
-	animation.AddKeyframe(
-		{ 1000,
-		{
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,90.0f}
-		}
-		});
-
-	animation.AddKeyframe(
-		{ 1000,
-		{
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,-90.0f}
-		}
-		});
-
-	animation.AddKeyframe(
-		{ 1000,
-		{
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,90.0f}
-		}
-		});
-
-	animation.AddKeyframe(
-		{ 1000,
-		{
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,0.0f},
-			glm::vec3{0.0f,0.0f,0.0f}
-		}
-		});
-
-	Entity object = { model_matrix, {vbo,ib,texture}, animation };
-
-	bool show = true;
-
-	uint64_t credit_count = 0;
-	std::atomic_uint64_t play_count = 0;
-	uint64_t credits_extracted = 0;
-
-	animation.AddAnimationEndCallback([&play_count]()
-		{
-			play_count++;
-		});
+	Game game;
 
 	while (window.IsOpen())
 	{
@@ -141,40 +197,27 @@ int main()
 		bool start_pressed = ImGui::Button("Start");
 		if (start_pressed)
 		{
-
-			if (animation.animating)
-			{
-				object.animation.PauseResumeToggle();
-			}
-			else
-			{
-				if (credit_count > 0)
-				{
-					credit_count--;
-					object.animation.Play();
-				}
-			}
+			game.Start();
 		}
 
 		bool credit_in_pressed = ImGui::Button("Credits In");
 		if (credit_in_pressed)
 		{
-			credit_count++;
+			game.CreditsIn();
 		}
 
 		bool credits_out_pressed = ImGui::Button("Credits Out");
 		if (credits_out_pressed)
 		{
-			credits_extracted += credit_count;
-			credit_count = 0;
+			game.CreditsOut();
 		}
 
 		ImGui::EndMainMenuBar();
 
 		ImGui::Begin("Information Board");
-		std::string move_count_text = "Number Of Plays:" + std::to_string(play_count);
-		std::string curr_credits_text = "Current Credits:" + std::to_string(credit_count);
-		std::string extracted_credits_text = "Extracted Credits:" + std::to_string(credits_extracted);
+		std::string move_count_text = "Number Of Plays:" + std::to_string(game.play_count);
+		std::string curr_credits_text = "Current Credits:" + std::to_string(game.credit_count);
+		std::string extracted_credits_text = "Extracted Credits:" + std::to_string(game.credits_extracted);
 		ImGui::Text(move_count_text.c_str());
 		ImGui::Text(curr_credits_text.c_str());
 		ImGui::Text(extracted_credits_text.c_str());
@@ -182,8 +225,7 @@ int main()
 
 		ImGuiRender();
 
-		shader.Bind();
-		object.Draw(shader,"model_matrix");
+		game.Draw(shader,"model_matrix");
 
 		window.Update();
 	}
