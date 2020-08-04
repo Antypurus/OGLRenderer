e53832ed451f866625c154b72adbeb4abf42b99d
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-08-04 01:46:55 +0100

    working on animation system

--- Renderer/Animation/Animation.cpp
+++ Renderer/Animation/Animation.cpp
@@ -1,6 +1,8 @@
 #include "Animation.h"
+#include <thread>
+#include <chrono>
 
-Keyframe::Keyframe(const uint32_t& delta_t, const Transform& transform_delta)
+Keyframe::Keyframe(const uint64_t& delta_t, const Transform& transform_delta)
 :delta_t(delta_t),transform_delta(transform_delta)
 {
 }
@@ -8,6 +10,7 @@ Keyframe::Keyframe(const uint32_t& delta_t, const Transform& transform_delta)
 void Animation::AddKeyframe(const Keyframe& keyframe)
 {
 	this->keyframes.push_back(keyframe);
+	this->duration += keyframe.delta_t;
 	if(this->keyframes.size() == 1)
 	{
 		// NOTE(Tiago): fist keyframe added. Start time = 0
@@ -15,19 +18,54 @@ void Animation::AddKeyframe(const Keyframe& keyframe)
 	}else
 	{
 		// NOTE(Tiago): not the first keyframe. Start time = Start Time of Previous Keyframe + previous keyframe duration
-		const uint32_t keyframe_index = this->keyframes.size() - 1;
-		const uint32_t start_time = this->keyframes[keyframe_index-1].start_time + this->keyframes[keyframe_index-1].delta_t; 
+		const size_t keyframe_index = this->keyframes.size() - 1;
+		const uint64_t start_time = this->keyframes[keyframe_index-1].start_time + this->keyframes[keyframe_index-1].delta_t; 
 		this->keyframes[keyframe_index].start_time = start_time;
 	}
 }
 
 void Animation::Play()
 {
+	using namespace std::chrono;
+
 	this->playback_head = 0;
 	this->current_keyframe_index = 0;
+
+	std::thread play_thread([this](){
+			high_resolution_clock::time_point prev_time = high_resolution_clock::now();
+			while(this->playback_head <= this->duration)
+			{
+				high_resolution_clock::time_point curr_time = high_resolution_clock::now();
+				uint64_t delta = duration_cast<std::chrono::nanoseconds>(curr_time - prev_time).count();
+
+				//updates playback head
+				this->playback_head += delta;
+
+				// checks if animation should end
+				if(this->playback_head > this->duration)
+				{
+					return;
+				}
+
+				//updates the current keyframe
+				while(this->playback_head > this->keyframes[this->current_keyframe_index].start_time)
+				{
+					this->current_keyframe_index++;
+				}
+
+				//compute the current transform
+				this->current_transform = Animation::Interpolate(
+					this->keyframes[this->current_keyframe_index],
+					this->keyframes[this->current_keyframe_index + 1],
+					this->playback_head);
+
+				prev_time = high_resolution_clock::now();
+			}
+		});
+	play_thread.detach();
 }
 
-Transform Animation::Interpolate(const Keyframe& start_keyframe, const Keyframe& end_keyframe, uint32_t playback_head)
+Transform Animation::Interpolate(const Keyframe& start_keyframe, const Keyframe& end_keyframe, uint64_t playback_head)
 {
 	return {};
 }
--- Renderer/Animation/Animation.h
+++ Renderer/Animation/Animation.h
@@ -6,18 +6,19 @@
 
 struct Keyframe
 {
-	uint32_t start_time = 0;
-	uint32_t delta_t = 0;
+	uint64_t start_time = 0;
+	uint64_t delta_t = 0;
 	Transform transform_delta;
 	
 	Keyframe() = default;
-	Keyframe(const uint32_t& delta_t, const Transform& transform_delta);
+	Keyframe(const uint64_t& delta_t, const Transform& transform_delta);
 };
 
 struct Animation
 {
-	uint32_t playback_head = 0;
-	uint32_t current_keyframe_index =0;
+	uint64_t playback_head = 0;
+	uint64_t current_keyframe_index =0;
+	uint64_t duration = 0;
 	std::vector<Keyframe> keyframes;
 	Transform current_transform;
 	
@@ -26,5 +27,5 @@ struct Animation
 	void AddKeyframe(const Keyframe& keyframe);
 	void Play();
 	
-	static Transform Interpolate(const Keyframe& start_keyframe, const Keyframe& end_keyframe, uint32_t playback_head);
+	static Transform Interpolate(const Keyframe& start_keyframe, const Keyframe& end_keyframe, uint64_t playback_head);
 };
